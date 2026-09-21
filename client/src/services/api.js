5960c7f0872976ab53e020b194d3e78016b7877b
satyamprabhat296 <satyam7015@gmail.com>
2026-09-22 00:03:25 +0530

    Prepare frontend for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
 });
+  
 
 export default api;
